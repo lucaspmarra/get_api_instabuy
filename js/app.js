179894bbf980a4b6e58325aff9cbe82363715143
Lucas Marra <marralucas1@gmail.com>
2020-07-27 19:48:16 -0300

    deleted axios to use fetch()

--- js/app.js
+++ js/app.js
@@ -36,35 +36,3 @@ new Vue({
       },
 
 })
-// .then((response) => {
-//     this.collection_items.push(
-//         response.data.data.collection_items[0].items[0]
-//     )
-// })
-// this.collection_items1 = response.data.data.collection_items[0].title
-
-// // URL da api
-// const API_URL = 'https://api.instabuy.com.br/apiv3/layout';
-// axios.get(API_URL, {
-//         params: {
-//             subdomain: 'organicos'
-//         }
-//     })
-//     .then(response => {
-//         for (let produtos in response.data.data.collection_items) {
-//             app.innerHTML = `
-//            <h1>ID: ${response.data.data.collection_items[produtos].items} </h1>
-//            `;
-//             // console.log(response.data.data.collection_items[produtos].items);
-//         }
-//     })
-
-// // .then(function (response) {
-// //     console.log(response.data.data.collection_items[0].items);
-// // })
-// // .catch(function (error) {
-// //     console.log('Erro de requisição:', error);
-// // })
-// // .then(function () {
-// //     // always executed
-// // });
--- js/request.js
+++ js/request.js
@@ -1,22 +1,29 @@
 const API_URL = 'https://api.instabuy.com.br/apiv3/layout?subdomain=organicos';
 // url da api a ser consultada
-const bannerElement = document.querySelector('.banner');
-const showData = document.querySelector('.show-data');
+fetch(API_URL)
+    .then(response => response.json())
+    .then(api_data => {
+        const lista_produtos = api_data.data.collection_items
+        // retorna lista de produtos do array
+        document.getElementById('lista_produtos').innerHTML =
+            `${lista_produtos.map(produto => (
+            produto.items.map(item =>(
+                `<div class="uk-card uk-card-small uk-card-default uk-card-hover uk-card-body uk-card-body-rounded">
+                
+                <div key=${item.id}></div>
+                <p class="uk-text-center">${item.name}</p>
+                <img class="uk-border-circle" src=${`https://assets.instabuy.com.br/ib.item.image.large/l-${item.images[0]}`}
+                    alt=${item.name} />
+                    
+                <p class="uk-text-center">Por apenas: R$ ${item.prices[0].price}</p>
+                </br>
+                <div>
+                <button class="uk-button uk-button-danger uk-position-bottom-center">Comprar</button>
+                </div>
+                
+                
+            </div>`
+            )).join('')
+        )).join('')}`
 
-async function getBanners() {
-    const response = await fetch(API_URL);
-    const json = await response.json();
-    console.log('Nome: ' + json.data.banners[0].title, 'com a ID: ' + json.data.banners[1].id);
-    // item 1
-    console.log('Nome: ' + json.data.banners[1].title, 'com a ID: ' + json.data.banners[1].id);
-    // item 1
-    // item 2
-    console.log('A ' + json.data.collection_items[0].title, 'vende: ' + json.data.collection_items[0].items[0].name, 'por R$: ' + json.data.collection_items[0].items[0].prices[0].price);
-    // item 2
-    // item 3
-    console.log('Nome: ' + json.data.collection_items[1].title);
-    // item 3
-}
-
-// getBanners();
-showData.addEventListener('click', getBanners);
+    })
